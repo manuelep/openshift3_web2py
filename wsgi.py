9ced626d08dec84b651e2548a8f56a9c031cb27e
Manuele Pesenti <manuele@inventati.org>
2017-09-01 15:45:09 +0200

    debug

--- wsgi.py
+++ wsgi.py
@@ -13,6 +13,8 @@ os.environ['web2py_path'] = path_to_web2py
 sys.path.append(path_to_web2py)
 sys.path.append(os.path.join(path_to_web2py, 'gluon'))
 
+os.mkdir(os.path.join(path_to_web2py, 'logs'))
+
 from gluon.settings import global_settings
 from gluon.main import appfactory
 WEB2PY_LOG = os.path.join(here, 'log', 'web2py.log')
